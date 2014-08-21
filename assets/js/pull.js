ff5c634f1d135096e5dc5d41a719598da40c2de3
nabdel8@uic.edu <>
2014-08-21 17:42:14 +0000

    init

---
+++ assets/js/pull.js
@@ -0,0 +1,6 @@
+$.getJSON('http://data.cityofchicago.org/resource/8pix-ypme.json', function (lstops){
+  $('#lstoplist').empty();
+   $.each(lstops, function (i,lstop) {$('#lstoplist').append(generateLStopLink(lstop));
+   });
+   $('#lstoplist').refresh();
+})
